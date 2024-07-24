2a73388e704fef40e60955308921bc8eb35aab2c
sergiorbotelho <sergiorbotelho@gmail.com>
2024-07-23 22:22:52 -0300

    feat: Add menu component

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,56 +1,81 @@
 import { Tabs } from "expo-router";
-import { Foundation, Ionicons } from "@expo/vector-icons";
+import { FontAwesome5, Foundation, Ionicons } from "@expo/vector-icons";
 import { theme } from "@/theme";
 import { Avatar } from "@/components/Avatar";
+import { useRef } from "react";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { View } from "react-native";
+import { Menu } from "@/components/Menu";
 export default function Layout() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  const handleBottomSheetOpen = () => bottomSheetRef.current?.expand();
+  const handleBottomSheetClose = () => bottomSheetRef.current?.snapToIndex(0);
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: theme.colors.white,
-        tabBarInactiveTintColor: theme.colors.gray[600],
-        tabBarStyle: {
-          backgroundColor: theme.colors.black,
-          borderTopWidth: 0,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarIcon: ({ size, color }) => (
-            <Foundation name="home" size={size} color={color} />
-          ),
+    <View style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: theme.colors.white,
+          tabBarInactiveTintColor: theme.colors.gray[600],
+          tabBarStyle: {
+            backgroundColor: theme.colors.black,
+            borderTopWidth: 0,
+          },
         }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          tabBarIcon: ({ size, color }) => (
-            <Ionicons name="search" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="messages"
-        options={{
-          tabBarIcon: ({ size, color }) => (
-            <Ionicons name="chatbubble-ellipses" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <Avatar
-              selected={color === theme.colors.white}
-              source={{ uri: "https://github.com/sergiorbotelho.png" }}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Foundation name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Ionicons name="search" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="menu"
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <FontAwesome5 name="plus" size={size} color={color} />
+            ),
+          }}
+          listeners={() => ({
+            tabPress: (event) => {
+              event.preventDefault();
+              handleBottomSheetOpen();
+            },
+          })}
+        />
+        <Tabs.Screen
+          name="messages"
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Ionicons name="chatbubble-ellipses" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Avatar
+                selected={color === theme.colors.white}
+                source={{ uri: "https://github.com/sergiorbotelho.png" }}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+      <Menu ref={bottomSheetRef} onClose={handleBottomSheetClose} />
+    </View>
   );
 }
